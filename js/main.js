21d0a1b07e79ea7ec2e793e9e77e68de2de0a86a
Anton <anton.ohlin@hotmail.com>
2019-11-07 15:26:29 +0100

    vet inte ens vad jag har gjort nu

--- js/main.js
+++ js/main.js
@@ -13,10 +13,25 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-//home
+// function update(e){
+//   var x = e.clientX || e.touches[0].clientX
+//   var y = e.clientY || e.touches[0].clientY
+
+//   document.documentElement.style.setProperty('--cursorX', x + 'px')
+//   document.documentElement.style.setProperty('--cursorY', y + 'px')
+// }
+
+// document.addEventListener('mousemove',update)
+// document.addEventListener('touchmove',update)
+
+
+
+// home
 $(document).on('click', '.lightbulb', () => {
 $('.picswap > img, .startlinks, .navbar').toggleClass('hidden');
 });
+
+
 
 // Kontaktformulär.
 (function() {
